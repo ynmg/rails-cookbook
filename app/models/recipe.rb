9c46a77ff10815f6c2955434beefd329822fb150
Nga Man Yeung(Grace) <graceyeungnm@gmail.com>
2025-05-12 11:06:28 +0100

    seeded 4 recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,5 @@ class Recipe < ApplicationRecord
 
   validates :name, uniqueness: true
   validates :description, presence: true
-  validates :rating, numericality: inclusion { in: 0..5, message: "must be between 0 and 5"}
+  validates :rating, numericality: true, inclusion: { in: 0.0..5.0, message: "must be between 0 and 5" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# name description image_url rating
+
+Recipe.destroy_all
+
+puts "Creating recipes..."
+egg_fried_rice = Recipe.create!(name: "egg fried rice", description: "Make your own healthy egg-fried rice with our easy recipe. Use leftover rice, or cook and dry it on a plate before using so it doesn't stick to the wok", image_url: "https://www.seriouseats.com/thmb/zO80j7KGl3j2k3vgrNVahBWUQBk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20230529-SEA-EggFriedRice-AmandaSuarez-hero-c8d95fbf69314b318bc279159f582882.jpg", rating: 4.1)
+strawberry_smoothie = Recipe.create!(name: "strawberry smoothie", description: "A creamy breakfast-friendly blend that's high in calcium and low in calories", image_url: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/berry-smoothie-5543fc2.jpg?quality=90&webp=true&resize=440,400", rating: 4)
+banana_smoothie = Recipe.create!(name: "banana smoothie", description: "A creamy breakfast-friendly blend that's high in calcium and low in calories", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLw1Ou8CS7Iam0jc9l9MpMeJHLnZ0oA9a_n4kzfVzry4S7q2E1AA&s=10&ec=72940545", rating: 4.5)
+avocado_smoothie = Recipe.create!(name: "avocado smoothie", description: "A creamy breakfast-friendly blend that's high in calcium and low in calories", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZnoxoHbPsWUm6TbdR0Ahbv9oiq5XErIGI0oDnqRuFva-WKA4Osw&s=10&ec=72940545", rating: 3.9)
+
+puts "#{Recipe.count} recipes created!"
